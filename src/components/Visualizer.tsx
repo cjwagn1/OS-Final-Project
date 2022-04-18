1af4eee0fa2723993ae3b03e1b12dcdde2557c69
Carter Wagner <cjwagn1@gmail.com>
2022-04-17 19:43:39 -0500

    Added animation library, added styled components and fixed typescript issues

---
+++ src/components/Visualizer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled, { AnyStyledComponent } from "styled-components";
+
+const Example: AnyStyledComponent = styled.div``;
+
+export default () => {
+  return <Example>This is an example component</Example>;
+};
