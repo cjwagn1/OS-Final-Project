330f860e5b24e1837bdac6315327c83af72367e8
Carter Wagner <cjwagn1@gmail.com>
2022-04-17 20:08:14 -0500

    Added animation basics

--- src/components/common/Visualizer.tsx
+++ src/components/common/Visualizer.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import styled, { AnyStyledComponent } from "styled-components";
+import { motion } from "framer-motion";
 
 const Example: AnyStyledComponent = styled.div``;
 
 export default () => {
-  return <Example>This is an example component</Example>;
+  return (
+    <div>
+      <motion.div animate={{ scale: 2 }} transition={{ duration: 0.5 }}>
+        Animation
+      </motion.div>
+    </div>
+  );
 };
