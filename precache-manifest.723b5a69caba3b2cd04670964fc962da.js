d45e7582768755d769f79b4ad20e1348d580fcf0
Carter Wagner <cjwagn1@gmail.com>
2022-04-25 02:54:50 -0500

    Updates

---
+++ precache-manifest.723b5a69caba3b2cd04670964fc962da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a94b917234d4840f9d81f167d1c046e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "768da9b8022d32850431",
+    "url": "/static/css/2.666ddd5e.chunk.css"
+  },
+  {
+    "revision": "b2f5395b34aef4b84b28",
+    "url": "/static/css/main.f6997683.chunk.css"
+  },
+  {
+    "revision": "768da9b8022d32850431",
+    "url": "/static/js/2.367150fc.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.367150fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2f5395b34aef4b84b28",
+    "url": "/static/js/main.38500d17.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
